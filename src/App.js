0ff7839698063c6881972a818b17af3fa3c881c5
Daniel Hong <hongchangyeon@gmail.com>
2021-09-19 23:13:05 -0500

    Added movie Card component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import { MyNavBar } from './Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { MyNavBar } from "./Components/Header";
+import { MovieCard } from "./Components/MovieCard";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <MyNavBar/>
-
+      <MyNavBar />
+      <MovieCard />
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,34 @@
-import React from "react"
-import { Nav, Navbar, Container, Form, FormControl, Button } from "react-bootstrap"
+import React from "react";
+import {
+  Nav,
+  Navbar,
+  Container,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 export class MyNavBar extends React.Component {
-    render() {
-        return (
-            <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">TVMAZE</Navbar.Brand>
-    <Nav className="me-auto">
-        <Nav.Link href="#Home">Home</Nav.Link>      
-      <Nav.Link href="#Shows">Shows</Nav.Link>
-    </Nav>
-    <Form className="d-flex">
-      <FormControl
-        type="search"
-        placeholder="Search for Shows"
-        className="mr-2"
-        aria-label="Search"
-      />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-    </Container>
-  </Navbar>
-        );
-    }
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">TVMAZE</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#Shows">Shows</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search for Shows"
+              className="mr-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+    );
+  }
 }
