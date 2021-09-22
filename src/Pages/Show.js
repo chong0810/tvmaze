841b67ce85433490d3c9c03d348da45c314d31c8
Daniel Hong <hongchangyeon@gmail.com>
2021-09-22 03:39:33 -0500

    changed pages

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Show = () => {
-    return (
-        <h1>
-        This is show page
-        </h1>
-    )
+class Show extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+
+    this.searchForMovie();
+
+  }
+
+  searchForMovie = () => {
+      
+    fetch("https://api.tvmaze.com/shows")
+
+  };
+
+  render() {
+    return <h1>This is a show page</h1>;
+  }
 }
 
-export default Show
+export default Show;
