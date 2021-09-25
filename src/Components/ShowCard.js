5cce650053ea2022e8a0ba0bb2c7509f615406ef
Daniel Hong <hongchangyeon@gmail.com>
2021-09-25 01:09:21 -0500

    README updated

--- src/Components/ShowCard.js
+++ src/Components/ShowCard.js
@@ -5,7 +5,7 @@ export class ShowCard extends React.Component {
   render() {
     return (
       <Card bg='dark' text='white' style={{ width: "18rem", margin:".5rem"  }}>
-        <Card.Img variant="top" src={this.props.details.image.medium} />
+        <a href= {this.props.details.url}><Card.Img variant="top" src={this.props.details.image.medium} /></a>
         <Card.Body>
           <Card.Title><a href= {this.props.details.url} > {this.props.details.name}</a> </Card.Title>
           <Card.Text>
