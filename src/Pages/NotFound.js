c5379e28656ead7816ac7bafd33afb4b69ff6030
Daniel Hong <hongchangyeon@gmail.com>
2021-09-24 22:54:35 -0500

    404 page edited

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 function NotFound() {
     return (
         <h1>
-            404 Not Found
+            404 Not Found. You tried to break this website!
         </h1>
     )
 }
